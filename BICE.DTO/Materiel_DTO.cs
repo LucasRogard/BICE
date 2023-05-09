3c8d4b526c3437251fd46f2bad380c45a8cf3d68
Yughi <yughi.bis@gmail.com>
2023-05-09 13:41:42 +0200

    ajout DTO

---
+++ BICE.DTO/Materiel_DTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DTO
+{
+    public class Materiel_DTO
+    {
+        public int Id { get; set; }
+        public string Denomination { get; set; }
+        public string Categorie { get; set; }
+        public string Numero { get; set; }
+        public bool EstStocke { get; set; }
+        public int? NbUtilisation { get; set; }
+        public int? NbMaxUtilisation { get; set; }
+        public DateTime? DateExpiration { get; set; }
+        public DateTime? DateControle { get; set; }
+    }
+}
---
+++ BICE.DTO/Vehicule_DTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DTO
+{
+    public class Vehicule_DTO
+    {
+        public int Id { get; set; }
+        public string Denomination { get; set; }
+        public string? Immatriculation { get; set; }
+        public string Numero { get; set; }
+    }
+}
